e1b4d0a9fe5211e5bed9beb486173bf0f7af3217
Michael Martins <michaelmartins096@gmail.com>
2020-05-11 02:19:46 -0300

    added: config initial root app

---
+++ lib/app/pages/root/root_app.dart
@@ -0,0 +1,11 @@
+import 'package:covid_caru/app/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class RootApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
